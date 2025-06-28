98c153dbbb6a7fb6e5419ada421a588677eb07a1
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-06-28 14:45:56 +0100

    the legacy way of connecting components to redux

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,4 +1,6 @@
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
+
+import { connect } from 'react-redux'
 
 function formatCurrency(value) {
   return new Intl.NumberFormat('en', {
@@ -7,10 +9,16 @@ function formatCurrency(value) {
   }).format(value)
 }
 
-function BalanceDisplay() {
-  const balance = useSelector((store) => store.account.balance)
+function BalanceDisplay({ balance }) {
+  // const balance = useSelector((store) => store.account.balance)
 
   return <div className='balance'>{formatCurrency(balance)}</div>
 }
 
-export default BalanceDisplay
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  }
+}
+
+export default connect(mapStateToProps)(BalanceDisplay)
